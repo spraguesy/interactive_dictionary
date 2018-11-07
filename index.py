b4f2ac22609e4413b9031ba7533bffc583014a34
spraguesy <andrewsprague@outlook.com>
2018-11-07 08:28:42 -0600

    done

--- index.py
+++ index.py
@@ -1,21 +1,37 @@
 import json
 from difflib import SequenceMatcher as sm
+from difflib import get_close_matches as gcm
 
 data = json.load(open("./data.json"))
 
 
 def find_definition(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif len(gcm(word, data, n=1)) > 0:
+        suggestion = gcm(word, data, n=1)
+        response = input("Did you mean %s? (Y/N) " % suggestion[0])
+        if response.lower() == "y" :
+            return data[suggestion[0]]
+        elif response.lower() == "n":
+            return "I don't know what word you are looking for. Please try again."
     else:
-        return "Word does not exist."
+        return "The word doesn't exist. Please double check it."
+    
 
 
+def get_word():
+    return input("Enter a word: ")
 
 
-word = input("Find a word:")
-word = word.lower()
-print(sm(None, "rain", word).ratio())
-
-
-print(find_definition(word))
+def run_program():
+    word = get_word()
+    response = find_definition(word)
+    if type(response) is list:
+        for index, definition in enumerate(response):
+            print("%s. %s" % (index + 1, definition))
+    else:
+        print(response)
+            
+run_program()
